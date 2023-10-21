3d844529dfa71071cd7b096e6d248ae8f3bf4985
Alexander Evans <sander7989@gmail.com>
2023-10-20 22:24:55 -0600

    add favourite button to photolistitem

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,16 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [liked, setLiked] = useState(false);
+
+  const handleClick = () => {
+    setLiked(!liked);
+  } 
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={handleClick}>
+        <FavIcon displayAlert={false} selected={liked}/>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   return (
-    <section className="photoListItem">
-      <img src={props.imageSource}></img>
-      <div className="itemInfoBar">
-        <img src={props.profile}></img>
-        <span>{props.username}</span>
-        <span>
-          {props.location.city}, 
-          {props.location.country}
-        </span>
+    <section className="photo-list__item">
+      <PhotoFavButton/>
+      <img className="photo-list__image" src={props.imageSource}></img>
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.profile}></img>
+        <div className="photo-list__user-info">
+          <span>{props.username}</span>
+          <br></br>
+          <span className="photo-list__user-location">
+            {props.location.city}, {props.location.country}
+          </span>
+        </div>
       </div>
     </section>
   );
